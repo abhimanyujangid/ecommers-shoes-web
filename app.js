a76a7de3f0f9ab3154cef00ae7387a8e8b8cdc07
Abhimanyu Jangid <abhimanyujangid79@gmail.com>
2024-03-27 16:37:36 +0530

    add payment box

--- app.js
+++ app.js
@@ -3,101 +3,120 @@ const menuitem = document.querySelectorAll(".menuitem")
 
 
 const products = [
-    {
-      id: 1,
-      title: "Air Force",
-      price: 25000,
-      colors: [
-        {
-          code: "black",
-          img: "./img/air.png",
-        },
-        {
-          code: "darkblue",
-          img: "./img/air2.png",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "Air Jordan",
-      price: 13000,
-      colors: [
-        {
-          code: "lightgray",
-          img: "./img/jordan.png",
-        },
-        {
-          code: "green",
-          img: "./img/jordan2.png",
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: "Blazer",
-      price: 8000,
-      colors: [
-        {
-          code: "lightgray",
-          img: "./img/blazer.png",
-        },
-        {
-          code: "green",
-          img: "./img/blazer2.png",
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "Crater",
-      price: 19000,
-      colors: [
-        {
-          code: "black",
-          img: "./img/crater.png",
-        },
-        {
-          code: "lightgray",
-          img: "./img/crater2.png",
-        },
-      ],
-    },
-    {
-      id: 5,
-      title: "Hippie",
-      price: 32000,
-      colors: [
-        {
-          code: "gray",
-          img: "./img/hippie.png",
-        },
-        {
-          code: "black",
-          img: "./img/hippie2.png",
-        },
-      ],
-    },
-  ];
-  let choosenproduct = products[0]
-  const currntproductimg = document.querySelector(".productimg")
-  const currntproducttitle = document.querySelector(".producttitle")
-  const currntproductprice = document.querySelector(".productprice")
-  const currntproductcolor = document.querySelectorAll(".color")
-  const currntproductsize = document.querySelectorAll(".size")
+  {
+    id: 1,
+    title: "Air Force",
+    price: 25000,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 13000,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 8000,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 19000,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 32000,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+let choosenproduct = products[0]
+const currntproductimg = document.querySelector(".productimg")
+const currntproducttitle = document.querySelector(".producttitle")
+const currntproductprice = document.querySelector(".productprice")
+const currntproductcolor = document.querySelectorAll(".color")
+const currntproductsize = document.querySelectorAll(".size")
 
-  menuitem.forEach((item , index)=>{
-    item.addEventListener("click" , () => {
-        //change the current slide
-        wrapper.style.transform =  `translateX(${-100 * index}vw)`;
-        //change the choose product
-        choosenproduct = products[index]
+menuitem.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    //change the choose product
+    choosenproduct = products[index]
 
-        //change text of currentproduct
-        currntproducttitle.textContent = choosenproduct.title
-        currntproductprice.textContent = "₹ " + choosenproduct.price
-        currntproductimg.src = choosenproduct.colors[0].img
-
-
+    //change text of currentproduct
+    currntproducttitle.textContent = choosenproduct.title;
+    currntproductprice.textContent = "₹ " + choosenproduct.price;
+    currntproductimg.src = choosenproduct.colors[0].img;
+    //assing new color
+    currntproductcolor.forEach((color, index) => {
+      color.style.backgroundcolor = choosenproduct.colors[index].code
     });
+  });
+});
+
+currntproductcolor.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currntproductimg.src = choosenproduct.colors[index].img
+  });
+});
+
+currntproductsize.forEach((size, index) => {
+  size.style.addEventListener("click", () => {
+    currntproductsize.forEach((size) => {
+      size.style.backgroundcolor = "black";
+      size.style.color = "white";
+    });
+    size.style.backgroundcolor = "black";
+    size.style.color = "white";
+  });
 });
