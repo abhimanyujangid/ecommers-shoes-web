0ff100d6c8e79919e4ba5b8508eb8d65c68faaf5
Abhimanyu Jangid <abhimanyujangid79@gmail.com>
2024-03-27 17:45:07 +0530

    complete

--- app.js
+++ app.js
@@ -79,44 +79,59 @@ const products = [
     ],
   },
 ];
-let choosenproduct = products[0]
-const currntproductimg = document.querySelector(".productimg")
-const currntproducttitle = document.querySelector(".producttitle")
-const currntproductprice = document.querySelector(".productprice")
-const currntproductcolor = document.querySelectorAll(".color")
-const currntproductsize = document.querySelectorAll(".size")
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
 
 menuitem.forEach((item, index) => {
   item.addEventListener("click", () => {
     //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
-    //change the choose product
-    choosenproduct = products[index]
 
-    //change text of currentproduct
-    currntproducttitle.textContent = choosenproduct.title;
-    currntproductprice.textContent = "₹ " + choosenproduct.price;
-    currntproductimg.src = choosenproduct.colors[0].img;
-    //assing new color
-    currntproductcolor.forEach((color, index) => {
-      color.style.backgroundcolor = choosenproduct.colors[index].code
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "₹ " + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
     });
   });
 });
 
-currntproductcolor.forEach((color, index) => {
+currentProductColors.forEach((color, index) => {
   color.addEventListener("click", () => {
-    currntproductimg.src = choosenproduct.colors[index].img
+    currentProductImg.src = choosenProduct.colors[index].img;
   });
 });
 
-currntproductsize.forEach((size, index) => {
-  size.style.addEventListener("click", () => {
-    currntproductsize.forEach((size) => {
-      size.style.backgroundcolor = "black";
-      size.style.color = "white";
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
     });
-    size.style.backgroundcolor = "black";
+    size.style.backgroundColor = "black";
     size.style.color = "white";
   });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
 });
